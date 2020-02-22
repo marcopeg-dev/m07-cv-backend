bc85ccfc0e90c2bd7b3151fe83d6f7a65d918427
Ömer <omerfsozcu@gmail.com>
2020-02-22 13:16:12 +0000

    fix/15

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ server.get("/:uname", async (request, reply) => {
     const result = await client.query(sql, [request.params.uname]);
 
     if (!result.rowCount) {
-        reply.code(404).send("The username was not found");
+        reply.code(404).send("The username was not found.");
         return;
     }
 
